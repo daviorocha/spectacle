304f6963d6cb1910efbe201dec9f42eb6abbd97c
Jared Mellentine <jared@mellentine.com>
2017-09-06 10:55:20 -0700

    Added 1/4 and 3/4 to left and right window calculations

--- Spectacle/Resources/Window Position Calculations/SpectacleLeftHalfWindowCalculation.js
+++ Spectacle/Resources/Window Position Calculations/SpectacleLeftHalfWindowCalculation.js
@@ -2,14 +2,31 @@ windowPositionCalculationRegistry.registerWindowPositionCalculationWithAction(fu
     var oneHalfRect = SpectacleCalculationHelpers.copyRect(visibleFrameOfDestinationScreen);
     oneHalfRect.width = Math.floor(oneHalfRect.width / 2.0);
     if (Math.abs(CGRectGetMidY(windowRect) - CGRectGetMidY(oneHalfRect)) <= 1.0) {
-        var twoThirdRect = SpectacleCalculationHelpers.copyRect(oneHalfRect);
-        twoThirdRect.width = Math.floor(visibleFrameOfDestinationScreen.width * 2 / 3.0);
+        var oneThirdsRect = SpectacleCalculationHelpers.copyRect(oneHalfRect);
+        oneThirdsRect.width = Math.floor(visibleFrameOfDestinationScreen.width / 3.0);
+                                                                              
+        var twoThirdsRect = SpectacleCalculationHelpers.copyRect(oneThirdsRect);
+        twoThirdsRect.width = Math.floor(visibleFrameOfDestinationScreen.width * 2 / 3.0);
+
+        var threeQuartersRect = SpectacleCalculationHelpers.copyRect(oneHalfRect);
+        threeQuartersRect.width = Math.floor(visibleFrameOfDestinationScreen.width * 3.0 / 4.0);
+
+        var oneQuartersRect = SpectacleCalculationHelpers.copyRect(oneHalfRect);
+        oneQuartersRect.width = Math.floor(visibleFrameOfDestinationScreen.width / 4.0);
+                                                                              
         if (SpectacleCalculationHelpers.rectCenteredWithinRect(oneHalfRect, windowRect)) {
-            return twoThirdRect;
+            return twoThirdsRect;
         }
-        if (SpectacleCalculationHelpers.rectCenteredWithinRect(twoThirdRect, windowRect)) {
-            var oneThirdsRect = SpectacleCalculationHelpers.copyRect(oneHalfRect);
-            oneThirdsRect.width = Math.floor(visibleFrameOfDestinationScreen.width / 3.0);
+                                                                              
+        if (SpectacleCalculationHelpers.rectCenteredWithinRect(twoThirdsRect, windowRect)) {
+            return threeQuartersRect;
+        }
+
+        if (SpectacleCalculationHelpers.rectCenteredWithinRect(threeQuartersRect, windowRect)) {
+            return oneQuartersRect;
+        }
+                                                                              
+        if (SpectacleCalculationHelpers.rectCenteredWithinRect(oneQuartersRect, windowRect)) {
             return oneThirdsRect;
         }
     }
--- Spectacle/Resources/Window Position Calculations/SpectacleRightHalfWindowCalculation.js
+++ Spectacle/Resources/Window Position Calculations/SpectacleRightHalfWindowCalculation.js
@@ -3,18 +3,39 @@ windowPositionCalculationRegistry.registerWindowPositionCalculationWithAction(fu
     oneHalfRect.width = Math.floor(oneHalfRect.width / 2.0);
     oneHalfRect.x += oneHalfRect.width;
     if (Math.abs(CGRectGetMidY(windowRect) - CGRectGetMidY(oneHalfRect)) <= 1.0) {
-        var twoThirdRect = SpectacleCalculationHelpers.copyRect(oneHalfRect);
-        twoThirdRect.width = Math.floor(visibleFrameOfDestinationScreen.width * 2 / 3.0);
-        twoThirdRect.x = visibleFrameOfDestinationScreen.x + visibleFrameOfDestinationScreen.width - twoThirdRect.width;
+        var oneThirdsRect = SpectacleCalculationHelpers.copyRect(oneHalfRect);
+        oneThirdsRect.width = Math.floor(visibleFrameOfDestinationScreen.width / 3.0);
+        oneThirdsRect.x = visibleFrameOfDestinationScreen.x + visibleFrameOfDestinationScreen.width - oneThirdsRect.width;
+        
+        var twoThirdsRect = SpectacleCalculationHelpers.copyRect(oneThirdsRect);
+        twoThirdsRect.width = Math.floor(visibleFrameOfDestinationScreen.width * 2 / 3.0);
+        twoThirdsRect.x = visibleFrameOfDestinationScreen.x + visibleFrameOfDestinationScreen.width - twoThirdsRect.width;
+        
+        var threeQuartersRect = SpectacleCalculationHelpers.copyRect(oneHalfRect);
+        threeQuartersRect.width = Math.floor(visibleFrameOfDestinationScreen.width * 3.0 / 4.0);
+        threeQuartersRect.x = visibleFrameOfDestinationScreen.x + visibleFrameOfDestinationScreen.width - threeQuartersRect.width;
+                                                                              
+        var oneQuartersRect = SpectacleCalculationHelpers.copyRect(oneHalfRect);
+        oneQuartersRect.width = Math.floor(visibleFrameOfDestinationScreen.width / 4.0);
+        oneQuartersRect.x = visibleFrameOfDestinationScreen.x + visibleFrameOfDestinationScreen.width - oneQuartersRect.width;
+
+        
         if (SpectacleCalculationHelpers.rectCenteredWithinRect(oneHalfRect, windowRect)) {
-            return twoThirdRect;
+            return twoThirdsRect;
         }
-        if (SpectacleCalculationHelpers.rectCenteredWithinRect(twoThirdRect, windowRect)) {
-            var oneThirdsRect = SpectacleCalculationHelpers.copyRect(oneHalfRect);
-            oneThirdsRect.width = Math.floor(visibleFrameOfDestinationScreen.width / 3.0);
-            oneThirdsRect.x = visibleFrameOfDestinationScreen.x + visibleFrameOfDestinationScreen.width - oneThirdsRect.width;
+        
+        if (SpectacleCalculationHelpers.rectCenteredWithinRect(twoThirdsRect, windowRect)) {
+            return threeQuartersRect;
+        }
+        
+        if (SpectacleCalculationHelpers.rectCenteredWithinRect(threeQuartersRect, windowRect)) {
+            return oneQuartersRect;
+        }
+
+        if (SpectacleCalculationHelpers.rectCenteredWithinRect(oneQuartersRect, windowRect)) {
             return oneThirdsRect;
         }
+                                                                              
     }
     return oneHalfRect;
 }, "SpectacleWindowActionRightHalf");
